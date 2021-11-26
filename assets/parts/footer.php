8aa0a2ece590a3eb26428b3d834d7e2b9c784a2f
Thomas <tdelorme@gaming.tech>
2021-11-26 16:08:29 +0100

    database

--- assets/parts/footer.php
+++ assets/parts/footer.php
@@ -1,96 +1,96 @@
 <!-- CONTACT -->
 <div class="container center contact">
-      <button data-target="modal-contact" class="btn-large modal-trigger grey darken-4 z-depth-3">Contact Us</button>
-    </div>
-    <div id="modal-contact" class="modal">
-      <div class="modal-content">
-        <section class="section container contact">
-          <h2 class="header center">~ Contact Us ~</h2>
-          <form>
-            <div class="input-field row">
-              <div class="col s12 m12 l6">
-                <i class="fas fa-user"></i>
-                <label for="fname">First Name</label>
-                <input type="text" id="fname">
-              </div>
-              <div class="col s12 m12 l6">
-                <i class="far fa-user"></i>
-                <label for="lname">Last Name</label>
-                <input type="text" id="lname">
-              </div>
-            </div>
-            <div class="input-field row">
-              <div class="col s12">
-                <i class="far fa-envelope"></i>
-                <label for="email">Your email</label>
-                <input type="email" id="email">
-              </div>
-              <div class="col s12">
-                <i class="fas fa-reply"></i>
-                <label for="text">Your message</label>
-                <textarea id="text" class="materialize-textarea" cols='30' rows='10'></textarea>
-              </div>
-            </div>
-          </form>
-        </section>
-      </div>
-      <div class="modal-footer">
-        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Submit</a>
-      </div>
-    </div>
-
-    <!-- FOOTER -->
-    <footer class="page-footer teal darken-4" id="footer">
-      <div class="container center">
-      <div class="row">
-        <div class="col s12 l2">
-          <h2 class="white-text">Arthur</h2>
-          <ul class="social">
-              <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='ajouve@gaming.tech (clic to copy)'><i class="far fa-envelope" onclick="copyToClipboard('#email-arthur')"></i></a></li>
-              <li><a href="https://github.com/Falaron" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Falaron (clic)'><i class="fab fa-github"></i></a></li>
-              <li><a href="https://www.linkedin.com/in/arthur-jouve-113293211/" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Arthur Jouve (clic)'><i class="fab fa-linkedin"></i></a></li>
-              <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Arthur~#0042 (clic to copy)'><i class="fab fa-discord" onclick="copyToClipboard('#discord-arthur')"></i></a></li>
-          </ul>
-        </div>
-        <div class="col s12 l3">
-        <h2 class="white-text">Thomas</h2>
-        <ul class="social">
-            <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='tdelorme@gaming.tech (clic to copy)'><i class="far fa-envelope" onclick="copyToClipboard('#email-thomas')"></i></a></li>
-            <li><a href="https://github.com/generaltoto" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='generaltoto (clic)'><i class="fab fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/thomas-delorme-94abb8221/" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Thomas Delorme (clic)'><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='general tøtø#5096 (clic to copy)' onclick="copyToClipboard('#discord-thomas')"><i class="fab fa-discord"></i></a></li>
-        </ul>
-        </div>
-        <div class="col s12 l2">
-          <h2 class="white-text">Menu</h2>
-          <ul>
-              <li><a href="index.html" class="grey-text text-lighten-3">Home</a></li>
-              <li><a href="project1.html" class="grey-text text-lighten-3">Destiny 2 project</a></li>
-              <li><a href="project2.html" class="grey-text text-lighten-3">HunterZ project</a></li>
-              <li><a href="project3.html" class="grey-text text-lighten-3">Beneath the sins project</a></li>
-          </ul>
-        </div>
-        <div class="col s12 l2">
-          <h2 class="white-text">Some Links</h2>
-          <ul>
-              <li><a href="https://bungie.net" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Destiny 2 official page</a></li>
-              <li><a href="https://hunterz-assaderie.com" target='_blank' class="grey-text text-lighten-3">HunterZ official page</a></li>
-              <li><a href="#" class="grey-text text-lighten-3" id="easteregg-color">Beneath the Sins Project</a></li>
-              <li><a href="https://gamingcampus.fr" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Gaming Campus</a></li>
-          </ul>
-        </div>
-        <div class="col s12 l3">
-          <h2 class="white-text">Legal Section</h2>
-          <div class="container">
-            <ul>
-                <li><a href="https://en.wikipedia.org/wiki/Privacy_policy" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Legal and privacy policy</a></li>
-                <li><a href="https://en.wikipedia.org/wiki/Terms_of_service" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Terms of service</a></li>
-            </ul>
+  <button data-target="modal-contact" class="btn-large modal-trigger grey darken-4 z-depth-3">Contact Us</button>
+</div>
+<div id="modal-contact" class="modal">
+  <div class="modal-content">
+    <section class="section container contact">
+      <h2 class="header center">~ Contact Us ~</h2>
+      <form>
+        <div class="input-field row">
+          <div class="col s12 m12 l6">
+            <i class="fas fa-user"></i>
+            <label for="fname">First Name</label>
+            <input type="text" id="fname">
+          </div>
+          <div class="col s12 m12 l6">
+            <i class="far fa-user"></i>
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname">
           </div>
         </div>
+        <div class="input-field row">
+          <div class="col s12">
+            <i class="far fa-envelope"></i>
+            <label for="email">Your email</label>
+            <input type="email" id="email">
+          </div>
+          <div class="col s12">
+            <i class="fas fa-reply"></i>
+            <label for="text">Your message</label>
+            <textarea id="text" class="materialize-textarea" cols='30' rows='10'></textarea>
+          </div>
+        </div>
+      </form>
+    </section>
+  </div>
+  <div class="modal-footer">
+    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Submit</a>
+  </div>
+</div>
+
+<!-- FOOTER -->
+<footer class="page-footer teal darken-4" id="footer">
+  <div class="container center">
+  <div class="row">
+    <div class="col s12 l2">
+      <h2 class="white-text">Arthur</h2>
+      <ul class="social">
+          <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='ajouve@gaming.tech (clic to copy)'><i class="far fa-envelope" onclick="copyToClipboard('#email-arthur')"></i></a></li>
+          <li><a href="https://github.com/Falaron" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Falaron (clic)'><i class="fab fa-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/arthur-jouve-113293211/" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Arthur Jouve (clic)'><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Arthur~#0042 (clic to copy)'><i class="fab fa-discord" onclick="copyToClipboard('#discord-arthur')"></i></a></li>
+      </ul>
+    </div>
+    <div class="col s12 l3">
+    <h2 class="white-text">Thomas</h2>
+    <ul class="social">
+        <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='tdelorme@gaming.tech (clic to copy)'><i class="far fa-envelope" onclick="copyToClipboard('#email-thomas')"></i></a></li>
+        <li><a href="https://github.com/generaltoto" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='generaltoto (clic)'><i class="fab fa-github"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/thomas-delorme-94abb8221/" target='_blank' rel="nofollow" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='Thomas Delorme (clic)'><i class="fab fa-linkedin"></i></a></li>
+        <li><a href="#" class="tooltipped btn-floating btn-small teal darken-3" data-tooltip='general tøtø#5096 (clic to copy)' onclick="copyToClipboard('#discord-thomas')"><i class="fab fa-discord"></i></a></li>
+    </ul>
+    </div>
+    <div class="col s12 l2">
+      <h2 class="white-text">Menu</h2>
+      <ul>
+          <li><a href="index.html" class="grey-text text-lighten-3">Home</a></li>
+          <li><a href="project1.html" class="grey-text text-lighten-3">Destiny 2 project</a></li>
+          <li><a href="project2.html" class="grey-text text-lighten-3">HunterZ project</a></li>
+          <li><a href="project3.html" class="grey-text text-lighten-3">Beneath the sins project</a></li>
+      </ul>
+    </div>
+    <div class="col s12 l2">
+      <h2 class="white-text">Some Links</h2>
+      <ul>
+          <li><a href="https://bungie.net" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Destiny 2 official page</a></li>
+          <li><a href="https://hunterz-assaderie.com" target='_blank' class="grey-text text-lighten-3">HunterZ official page</a></li>
+          <li><a href="#" class="grey-text text-lighten-3" id="easteregg-color">Beneath the Sins Project</a></li>
+          <li><a href="https://gamingcampus.fr" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Gaming Campus</a></li>
+      </ul>
+    </div>
+    <div class="col s12 l3">
+      <h2 class="white-text">Legal Section</h2>
+      <div class="container">
+        <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Privacy_policy" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Legal and privacy policy</a></li>
+            <li><a href="https://en.wikipedia.org/wiki/Terms_of_service" target='_blank' rel="nofollow" class="grey-text text-lighten-3">Terms of service</a></li>
+        </ul>
       </div>
-      <div class="footer-copyright">
-          <div class="container">&copy; All right reserved toto & tutur</div>
-      </div>
-      </div>
-  </footer>
+    </div>
+  </div>
+    <div class="footer-copyright">
+        <div class="container">&copy; All right reserved toto & tutur</div>
+    </div>
+  </div>
+</footer>
